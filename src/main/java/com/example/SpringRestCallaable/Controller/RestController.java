8d46bc2b73b7b232265a1180074c13da30f5800b
shiv sharma <shivkrsharma@gmail.com>
2019-10-20 23:37:55 +0100

    new change

--- src/main/java/com/example/SpringRestCallaable/Controller/RestController.java
+++ src/main/java/com/example/SpringRestCallaable/Controller/RestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -30,30 +31,30 @@ public class RestController {
     MovieService movieService;
 
     @Autowired
-     MyCallable callable;
+    MyCallable callable;
 
     @RequestMapping("/myApp")
     public String sayHi() throws InterruptedException {
 
-        System.out.println("Start time "+(System.currentTimeMillis())/100);
-      for(int i=0;i< 100; i++){
-          //restTemplate.getForObject("https://api.printful.com/countries", String.class);
+        System.out.println("Start time " + (System.currentTimeMillis()) / 100);
+        for (int i = 0; i < 100; i++) {
+            //restTemplate.getForObject("https://api.printful.com/countries", String.class);
 
-          CompletableFuture<MovieModel> page1 = movieService.lookForMovie("58611129-2dbc-4a81-a72f-77ddfc1b1b49");
+            CompletableFuture<MovieModel> page1 = movieService.lookForMovie("58611129-2dbc-4a81-a72f-77ddfc1b1b49");
        /*   ResponseEntity<Object> respponseEntity=
                   restOperations.exchange("https://api.printful.com/countries", HttpMethod.GET,null,Object.class);
        */  // System.out.println("Respnsec entity=="+respponseEntity);
-      }
-        System.out.println("End time "+(System.currentTimeMillis())/100);
-      return  "sucess";
+        }
+        System.out.println("End time " + (System.currentTimeMillis()) / 100);
+        return "sucess";
     }
 
     @RequestMapping("/newApp")
     public String sayHi1() throws ExecutionException, InterruptedException {
 
 
-        System.out.println("Start time "+(System.currentTimeMillis())/1000);
-        for(int i=0;i<100; i++){
+        System.out.println("Start time " + (System.currentTimeMillis()) / 1000);
+        for (int i = 0; i < 100; i++) {
 
             //Future<String> future = executorService.submit(callable);
             CompletableFuture<Object> page1 = movieService.lookForMovienew("58611129-2dbc-4a81-a72f-77ddfc1b1b49");
@@ -66,108 +67,93 @@ public class RestController {
         }*/
 
         //executorService.shutdown();
-        System.out.println("End time "+(System.currentTimeMillis())/1000);
+        System.out.println("End time " + (System.currentTimeMillis()) / 1000);
 
-        return  "sucess";
+        return "sucess";
     }
 
 
     @RequestMapping("/myAppTest")
     public String sayHiTest() throws InterruptedException {
 
-        System.out.println("Start time "+(System.currentTimeMillis())/100);
-        for(int i=0;i< 100; i++){
+        System.out.println("Start time " + (System.currentTimeMillis()) / 100);
+        for (int i = 0; i < 100; i++) {
             //restTemplate.getForObject("https://api.printful.com/countries", String.class);
-        String url = String.format("https://ghibliapi.herokuapp.com/films/%s", "58611129-2dbc-4a81-a72f-77ddfc1b1b49");
-        ResponseEntity<Object> respponseEntity=
-                  restOperations.exchange(url, HttpMethod.GET,null,Object.class);
-         // System.out.println("Respnsec entity=="+respponseEntity);
+            String url = String.format("https://ghibliapi.herokuapp.com/films/%s", "58611129-2dbc-4a81-a72f-77ddfc1b1b49");
+            ResponseEntity<Object> respponseEntity =
+                    restOperations.exchange(url, HttpMethod.GET, null, Object.class);
+            // System.out.println("Respnsec entity=="+respponseEntity);
         }
 
-        System.out.println("End time "+(System.currentTimeMillis())/100);
+        System.out.println("End time " + (System.currentTimeMillis()) / 100);
 
-        return  "sucess";
+        return "sucess";
     }
 
 
     @RequestMapping("/UserfromGitNoraml")
     public String getUserfromGitHub() throws InterruptedException {
 
-        System.out.println("End time "+(System.currentTimeMillis())/100);
-        for(int i=1;i< 100; i++){
+        System.out.println("End time " + (System.currentTimeMillis()) / 100);
+        for (int i = 1; i < 100; i++) {
             //restTemplate.getForObject("https://api.printful.com/countries", String.class);
             //String url = String.format("https://api.github.com/users/%s", i);
             String url = String.format("https://jsonplaceholder.typicode.com/todos/%s", i);
             System.out.println(url);
-            ResponseEntity<Object> respponseEntity=
-                    restOperations.exchange(url, HttpMethod.GET,null,Object.class);
+            ResponseEntity<Object> respponseEntity =
+                    restOperations.exchange(url, HttpMethod.GET, null, Object.class);
             // System.out.println("Respnsec entity=="+respponseEntity);
         }
 
-        System.out.println("End time "+(System.currentTimeMillis())/100);
+        System.out.println("End time " + (System.currentTimeMillis()) / 100);
 
-        return  "sucess";
+        return "sucess";
     }
 
     @RequestMapping("/UserfromGitNoramlSync")
     public String getUserfromGitHubSync() throws InterruptedException, ExecutionException {
 
         CompletableFuture<UserData> page1 = null;
-        System.out.println("End time "+(System.currentTimeMillis())/100);
-        Map<Integer,Integer> map1=  new HashMap<Integer,Integer>();
+        System.out.println("End time " + (System.currentTimeMillis()) / 100);
+        Map<Integer, Integer> map1 = new HashMap<Integer, Integer>();
 
         //List<CompletableFuture<ResponseEntity<UserData>>> completableFutures = new ArrayList<>(); //List to hold all the completable futures
-        CompletableFuture<ResponseEntity<UserData>> completableFutures =null; //List to hold all the completable futures
+        CompletableFuture<Integer> completableFutures = null; //List to hold all the completable futures
 
-        List<CompletableFuture<ResponseEntity<UserData>>> completedTasks = new ArrayList<>();
+        List<CompletableFuture<Integer>> completedTasks = new ArrayList<>();
 
         ExecutorService yourOwnExecutor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
         List<UserData> responses = new ArrayList<>();
 
-        Map<Integer, Integer> map21 =new HashMap<>();
-        map21.put(1,1);
-        map21.put(2,2);
-        map21.put(3,3);
-        map21.put(4,4);
+        Map<Integer, Integer> map21 = new HashMap<>();
+        map21.put(1, 1);
+        map21.put(2, 2);
+        map21.put(3, 3);
+        map21.put(4, 4);
 
 
         for (Map.Entry<Integer, Integer> entry : map21.entrySet()) {
-        completableFutures = CompletableFuture
-                .supplyAsync(() -> {
-                    try {
-                       return movieService.lookforUserfromGit(entry);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+            completableFutures = CompletableFuture
+                    .supplyAsync(() -> {
+                                try {
+                                    return movieService.lookforUserfromGit(entry);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
 
-                 return null;}
-                 );
-        //collect all async tasks
+                                return null;
+                            }
+                    );
+            //collect all async tasks
             completedTasks.add(completableFutures);
         }
 
-        System.out.println("--->>>"+completedTasks.get(0).get());
-
-        for (Map.Entry<Integer, Integer> entry : map21.entrySet()) {
-
-            CompletableFuture<ResponseEntity<UserData>> requestCompletableFuture = (CompletableFuture<ResponseEntity<UserData>>) CompletableFuture
-                    .supplyAsync(
-                            () ->
-
-                                    movieService.lookforUserfromGit(entry));
-
-            //completableFutures.add(requestCompletableFuture);
-
-
-            System.out.println("---->>"+requestCompletableFuture.get());
-
-            //completableFutures.add(requestCompletableFuture.get());
-        }
+        System.out.println("--->>>" + completedTasks.get(0).get());
 
 
         //System.out.println("size is"+completableFutures.get(0).get());
-     /*   CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0]))
+      /*  CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0]))
                 // avoid throwing an exception in the join() call
                 .exceptionally(ex -> null)
                 .join();
@@ -187,15 +173,50 @@ public class RestController {
         System.out.println(responseEntity.get().getBody());
        }
 */
-       System.out.println("End time "+(System.currentTimeMillis())/100);
+        System.out.println("End time " + (System.currentTimeMillis()) / 100);
 
-        return  "sucess";
+        return "sucess";
     }
 
 
+    @RequestMapping("/UserfromGitNoramlSync1")
+    public String getUserfromGitHubSync4() throws InterruptedException, ExecutionException {
 
 
+        CompletableFuture<UserData> page1 = null;
+        System.out.println("End time " + (System.currentTimeMillis()) / 100);
+        Map<Integer, Integer> map1 = new HashMap<Integer, Integer>();
 
+
+        ExecutorService newWorkStealingPool = Executors.newWorkStealingPool();
+        Instant start = Instant.now();
+        List<CompletableFuture<UserData>> pendingTasks = new ArrayList<>();
+        List<CompletableFuture<UserData>> completedTasks = new ArrayList<>();
+        CompletableFuture<UserData> task = null;
+
+        List<UserData> responses = new ArrayList<>();
+
+        Map<Integer, Integer> map21 = new HashMap<>();
+        map21.put(1, 1);
+        map21.put(2, 2);
+        map21.put(3, 3);
+        map21.put(4, 4);
+
+
+        System.out.println("Started doing things");
+        List<CompletableFuture> futures = new ArrayList();
+
+        for (Map.Entry<Integer, Integer> i : map21.entrySet()) {
+            futures.add(CompletableFuture.runAsync(() -> movieService.lookforUserfromGit1(i.getKey())));
+        }
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRunAsync(() -> System.out.println("Ended doing things"))
+        .get();
+
+
+        System.out.println(futures.get(1).get());
+     return "sucess";
+}
 
 
 
